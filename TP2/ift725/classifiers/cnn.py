3e24b1c65be60a85d29deb68cce358cddbc7a21c
Antoine GÉLIN <gelinantoine@gmail.com>
2020-02-28 11:22:13 -0500

    push buggé de 3LayersConvnet pushback before takeoff

--- TP2/ift725/classifiers/cnn.py
+++ TP2/ift725/classifiers/cnn.py
@@ -53,7 +53,14 @@ class ThreeLayerConvolutionalNet(object):
         # utilisez les clés 'W3' et 'b3' pour les poids et les biais de la couche  #
         # affine de sortie.                                                        #
         ############################################################################
+        self.params['W1'] = np.random.normal(0, weight_scale, input_dim * num_filters).reshape(input_dim, num_filters)
+        self.params['b1'] = np.zeros(num_filters)
 
+        self.params['W2'] = np.random.normal(0, weight_scale, num_filters * hidden_dim).reshape(num_filters, hidden_dim) 
+        self.params['b2'] = np.zeros(hidden_dim)
+
+        self.params['W3'] = np.random.normal(0, weight_scale, hidden_dim * num_classes).reshape(hidden_dim, num_classes) 
+        self.params['b3'] = np.zeros(num_classes)
 
         ############################################################################
         #                             FIN DE VOTRE CODE                            #
@@ -89,7 +96,17 @@ class ThreeLayerConvolutionalNet(object):
         #  couches, calculant les scores de classes pour X et stockez-les dans la  #
         #  variable scores.                                                        #
         ############################################################################
+        # Couche de convolution
+        layer_1, cache_layer_1 = forward_convolutional_relu_pool(X, self.params['W1'], self.params['b1'],conv_param,pool_param)
+        
+        # Couche cachee
+        layer_2, cache_layer_2 = forward_fully_connected(layer_1, self.params['W2'], self.params['b2'])
+        relu_2, cache_relu_2 = forward_relu(layer_2)
 
+        #Couche de sortie
+        layer_3, cache_layer_3 = forward_fully_connected(relu_2, self.params['W3'], self.params['b3'])
+
+        scores = layer_3
 
         ############################################################################
         #                             FIN DE VOTRE CODE                            #
@@ -107,6 +124,31 @@ class ThreeLayerConvolutionalNet(object):
         # grads[k] contient les gradients pour self.params[k]. N'oubliez pas       #
         # d'ajouter la régularisation L2!                                          #
         ############################################################################
+
+        # Softmax
+        loss, dx = softmax_loss(scores, y)
+
+        # Retro-propagation 3e couche
+        dx_layer_3, dw_layer_3, db_layer_3 = backward_fully_connected(dx, cache_layer_3)
+
+        # Retro-propagation 2e couche
+        dx_layer_2, dw_layer_2, db_layer_2 = backward_fully_connected(dx, cache_layer_2)
+        dx_relu_1 = backward_relu(dx_layer_2, cache_relu_2)
+
+
+        # Retro-propagation 1ere couche
+        dx_layer_1, dw_layer_1, db_layer_1 = backward_convolutional_relu_pool(dx_relu_1, cache_layer_1)
+
+        # Ajouter le terme de regularisation
+        grads['W1'] = dw_layer_1 + 0.5 * self.reg * self.params['W1']
+        grads['b1'] = db_layer_1 + 0.5 * self.reg * self.params['b1']
+        grads['W2'] = dw_layer_2 + 0.5 * self.reg * self.params['W2']
+        grads['b2'] = db_layer_2 + 0.5 * self.reg * self.params['b2']
+        grads['W2'] = dw_layer_3 + 0.5 * self.reg * self.params['W3']
+        grads['b2'] = db_layer_3 + 0.5 * self.reg * self.params['b3']
+
+        loss += self.reg * (np.linalg.norm(self.params['W1'])**2 + np.linalg.norm(self.params['b1'])**2 + np.linalg.norm(self.params['W2'])**2 + np.linalg.norm(self.params['b2'])**2 + np.linalg.norm(self.params['W3'])**2 + np.linalg.norm(self.params['b3'])**2)
+
 
         ############################################################################
         #                             FIN DE VOTRE CODE                            #
