5787e7474b2c6d378db365409aab264c18cf8b1d
Antoine GÉLIN <gelinantoine@gmail.com>
2020-03-17 15:11:16 -0400

    CNN blocks fini mais non testé

--- TP3/src/models/CNNBlocks.py
+++ TP3/src/models/CNNBlocks.py
@@ -41,3 +41,67 @@ class ResidualBlock(nn.Module):
         output = F.relu(output)
         return output
 
+
+
+
+
+
+
+
+class DenseBlock(nn.Module):
+    """
+    this block is the building block of the residual network. it takes an
+    input with in_channels, applies some blocks of convolutional layers
+    to reduce it to out_channels and sum it up to the original input,
+    """
+
+    def __init__(self, in_channels,out_channels):
+        super(DenseBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        
+        self.conv3 = nn.Conv2d(96, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+   
+
+    def forward(self, x):
+        c1 = F.relu(self.conv1(self.bn1(x)))
+        c2 = self.conv2(c1)
+        c2_dense = F.relu(torch.cat([c1, c2], 1))        
+        
+        c3 = self.conv3(c2_dense)
+        output = F.relu(c3)
+        return output
+
+
+
+
+
+
+class BottleneckBlock(nn.Module):
+    """
+    this block is the building block of the residual network. it takes an
+    input with in_channels, applies some blocks of convolutional layers
+    to reduce it to out_channels and sum it up to the original input,
+    """
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(BottleneckBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
+
+        self.bn3 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+
+    def forward(self, x):
+        c1 = F.relu(self.conv1(self.bn1(x)))
+        c2 = F.relu(self.conv2(self.bn2(c1)))
+        output = self.bn3(self.conv3(c2))
+        output = F.relu(output)
+        return output
